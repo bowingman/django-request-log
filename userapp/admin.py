89854a5d3e33981ed196dcfffab07aec158aebed
bowingman <geohinster.sag@gmail.com>
2023-01-21 07:43:05 -0500

    Added userapp serializers and models

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    exclude = ('password', 'is_superuser', 'is_active', 'groups',
+               'user_permissions', 'last_login', 'date_joined')
+    list_display = ('username', 'email', 'first_name', 'last_name')
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 # Register your models here.
